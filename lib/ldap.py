4ba1bd3e508d8d198b314e8532e7cc3c8ba13bd5
Lucas Costa Beyeler <lucas.costab@outlook.com>
2018-07-13 19:24:49 -0300

    Including a class to manage the LDAP files

--- lib/ldap.py
+++ lib/ldap.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from json import dumps, loads
 from ldap3 import Server, Connection, ALL, MODIFY_DELETE
 
 # Importing the Config Files
@@ -8,11 +9,40 @@ from config import config
 class LDAPConnection(object):
 
     # Initialize a connection with the LDAP Server
-    def __init__(self):
-        server = Server(environ['SERVER_URL'],use_ssl=False)
-        self.ldap = Connection(server,environ['USERNAME'],environ['PASSWORD'])
+    def __init__(self,**kwargs):
+        self.folder = kwargs['folder']
+        self.filename = kwargs['filename']
+        server = Server(config['LDAPSERVER'],use_ssl=False)
+        self.ldap = Connection(server,kwargs['LDAPADMIN'],kwargs['LDAPPASS'])
         self.ldap.bind()
 
     # Close the connection with the LDAP Server
     def __del__(self):
         self.ldap.unbind()
+
+    # Save the file inside the disk
+    def save(self,sbase,sfilter):
+        rst = self.ldap.search(search_base=sbase,search_filter=sfilter)
+        if rst:
+            with open(self.folder + self.filename,'w') as stream:
+                stream.write(json.dumps(rst))
+        else:
+            return False
+
+    # Load the file to restore a backup
+    def upload(self):
+        with open(self.folder + self.filename,'w') as stream:
+            rst = loads(stream.read())
+        return self.ldap.add(self.cn,self.objectClass,rst)
+
+    # Delete a entry before try to restore
+    def delete(self):
+        return self.ldap.delete(cn)
+
+class Account(LDAPConnection):
+
+    objectClass = []
+    cn = ""
+
+    def __init__(self):
+        pass
